edf78e5ff82f0572861aadf0e93af00d38f513a9
Rodrigo <rodrigo.msdos@gmail.com>
2023-01-26 19:33:13 -0300

    adicao de nova funcao

--- app/sortearNumero.js
+++ app/sortearNumero.js
@@ -5,7 +5,7 @@ const numeroSecreto= gerarNumeroAleatorio()
 function gerarNumeroAleatorio(){
 return parseInt(Math.random() * maiorValor + 1)
 }
-console.log('Número Secreto:',numeroSecreto)
+//console.log('Número Secreto:',numeroSecreto)
 const elementoMenorValor = document.getElementById('menor-valor')
 const elementoMaiorValor = document.getElementById('maior-valor')
 elementoMenorValor.innerHTML = menorValor
